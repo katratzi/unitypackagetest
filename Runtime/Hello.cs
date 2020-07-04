abaaaa7ad1353c74f26d06c435bd8312812dabd3
katratzi <ric_clifford@hotmail.com>
2020-07-04 17:53:46 +0100

    initial commit of package

---
+++ Runtime/Hello.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HelloWorld
+{
+
+    public class Hello : MonoBehaviour
+    {
+        public string logThis = "";
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Debug.Log("Hello World" + logThis);
+            }
+        }
+    }
+}
